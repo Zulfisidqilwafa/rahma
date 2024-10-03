809a477a300a50fb7570f5c6885939eb89a82858
zulfisidqilwafa <zulfisidiq25@gmail.com>
2024-10-03 20:42:22 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+});
